2086ed8efbfbf012430d295fcdf991de092fb9f4
tamilsa <161282438+tamilsa@users.noreply.github.com>
2025-01-17 10:47:37 +0530

    Update eks.tf

--- terraform-eks/eks.tf
+++ terraform-eks/eks.tf
@@ -20,3 +20,10 @@ module "eks" {
     }
 }
 
+resource "aws_ecr_repository" "my_ecr_repo" {
+  name                 = "my-ecr-repo"
+  image_tag_mutability = "MUTABLE"  # or "IMMUTABLE" based on your requirement
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
